766c0859ba8ce9794ebe399773a1cb3f27a4939a
rrigato <rrigato@horizon.csueastbay.edu>
2015-11-08 20:13:35 -0800

    Adding a cheat button to the project

---
+++ app/src/main/java/android/bignerdranch/com/geoquiz/CheatActivity.java
@@ -0,0 +1,16 @@
+package android.bignerdranch.com.geoquiz;
+
+import android.app.Activity;
+import android.bignerdranch.com.geoquiz.R;
+import android.os.Bundle;
+
+/**
+ * Created by Randy on 11/6/2015.
+ */
+public class CheatActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
